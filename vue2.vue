6e597990af16808c21e24c510858c3c4b6b89eb4
lmeniconi <luciano@asimov.cl>
2022-07-15 01:15:26 -0400

    v1

---
+++ vue2.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <h1>{{ state }}</h1>
+    <Example />
+  </div>
+</template>
+
+<script>
+import Example from "@/components/Example.vue";
+
+// Focus Directive
+const focus = {
+  mounted: (el) => el.focus(),
+};
+
+export default {
+  components: {
+    Example,
+  },
+
+  directives: {
+    focus,
+  },
+
+  provide: {
+    exampleProvide: "exampleMSG",
+  },
+
+  inject: ["exampleInject"],
+
+  props: ["exampleProp"],
+
+  emits: ["event1", "event2"],
+
+  data() {
+    return {
+      state: 1,
+    };
+  },
+
+  computed: {
+    computedState() {
+      return this.state ? "ON" : "Off";
+    },
+  },
+
+  watch: {
+    state(val) {
+      console.log(val);
+    },
+  },
+
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+
+  created() {
+    console.log("created");
+  },
+
+  beforeMount() {
+    console.log("beforeMount");
+  },
+
+  mounted() {
+    console.log("mount");
+  },
+
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+
+  updated() {
+    console.log("updated");
+  },
+
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+
+  unmounted() {
+    console.log("unmounted");
+  },
+
+  methods: {
+    increment() {
+      this.state++;
+    },
+  },
+};
+</script>
---
+++ vue3.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <h1>{{ state }}</h1>
+    <Example />
+  </div>
+</template>
+
+<script setup>
+import {
+  provide,
+  inject,
+  defineProps,
+  defineEmits,
+  ref,
+  reactive,
+  computed,
+  watch,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from "vue";
+
+// Components Import
+import Example from "@/components/Example.vue";
+
+// Directives
+const vFocus = {
+  mounted: (el) => el.focus(),
+};
+
+provide("exampleProvide", "exampleMSG");
+
+const message = inject("exampleInject");
+
+const props = defineProps(["exampleProp"]);
+
+defineEmits(["event1", "event2"]);
+
+// data() => equivalent to:
+const state = ref(1); // Any type
+const reactiveState = reactive({ value: 1 }); // Object types
+
+const computedState = computed(() => {
+  return state.value ? "ON" : "Off";
+});
+
+watch(state, (val) => {
+  console.log(val);
+});
+
+// On beforeCreate & created script...
+
+onBeforeMount(() => {
+  console.log("beforeMount");
+});
+
+onMounted(() => {
+  console.log("mount");
+});
+
+onBeforeUpdate(() => {
+  console.log("beforeUpdate");
+});
+
+onUpdated(() => {
+  console.log("update");
+});
+
+onBeforeUnmount(() => {
+  console.log("beforeUnmount");
+});
+
+onUnmounted(() => {
+  console.log("unmounted");
+});
+
+// methods: => equivalent to:
+function increment() {
+  state.value++;
+}
+</script>
